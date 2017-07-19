f9407ca03782ede808c67dc58edde3fd744a7b0a
jimmy.shu <jimmy.shu@104.com.tw>
2017-07-19 14:13:09 +0800

    0719- CXFTest add JsonTest

---
+++ parent/cxf/src/main/java/ch/test/cxf/JsonTest.java
@@ -0,0 +1,10 @@
+package ch.test.cxf;
+public class JsonTest {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
