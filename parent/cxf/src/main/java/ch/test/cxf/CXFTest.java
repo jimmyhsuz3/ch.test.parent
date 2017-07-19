3099bd3acc0d48caf4a13dce9dcd2cf6d8df0760
jimmy.shu <jimmy.shu@104.com.tw>
2017-07-19 14:12:18 +0800

    0719- CXFTest add ProfileView

--- parent/cxf/src/main/java/ch/test/cxf/CXFTest.java
+++ parent/cxf/src/main/java/ch/test/cxf/CXFTest.java
@@ -5,9 +5,42 @@ import com.e104.mongo.MongoService00;
 import com.e104.pdd.JobRecomm00;
 import com.e104.plus.service.impl.AccessRecordService00;
 import com.e104.plus.service.impl.AccessRecordServiceException_Exception;
+import java.io.IOException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import demo.spring.service.HelloWorldImplService;
 public class CXFTest {
 	private List<String> list = new ArrayList<String>();
+	public static void main(String[] args){
+		System.out.println(new CXFTest().testMongoServiceProfileView());
+	}
+	private String testMongoServiceProfileView(){
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode node = mapper.createObjectNode().set("$and", mapper.createArrayNode()
+				.add(mapper.createObjectNode().set("pid", mapper.createObjectNode().put("$gt", 100000)))
+				.add(mapper.createObjectNode().set("pid", mapper.createObjectNode().put("$lt", 200000))));
+		String jsonReturn = new MongoService00().getMongoService00HttpSoap11Endpoint().select("mdb0f00001", "ProfileView", node.toString());
+		ArrayNode ary = mapper.createArrayNode();
+		String JsonTestStr = "{\"name\":\"JsonTest\"}";
+		try {
+			JsonTest jsonTest = mapper.readValue(JsonTestStr, JsonTest.class);
+			ary.addPOJO(mapper.writeValueAsString(jsonTest));
+			ary.addPOJO(mapper.readTree(JsonTestStr));
+			JsonNode rootNode = mapper.readTree(jsonReturn);
+			for (int i = 0; i < rootNode.size(); i++)
+				ary.add(rootNode.get(i).get("pid").asLong());
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return ary.toString();
+	}
 	public List<String> testMongoService(){
 		try {
 			list.add(new MongoService00().getMongoService00HttpEndpoint().select("mdb0c00038", "jimmy_test", "{id:2}"));
@@ -17,6 +50,7 @@ public class CXFTest {
 		}
 		list.add(new MongoService00().getMongoService00HttpSoap11Endpoint().select("mdb0c00038", "jimmy_test", "{id:2}"));
 		list.add(new MongoService00().getMongoService00HttpSoap12Endpoint().select("mdb0c00038", "jimmy_test", "{id:2}"));
+		list.add(testMongoServiceProfileView());
 		return list;
 	}
 	public String testHelloWorld(){
