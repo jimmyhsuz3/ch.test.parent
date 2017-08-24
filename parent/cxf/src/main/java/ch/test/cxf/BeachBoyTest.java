17ecdfd0f0deedf6d0e484645b8f72ec6da78766
jimmy.shu <jimmy.shu@104.com.tw>
2017-08-24 11:20:55 +0800

    0824-modify: BeachBoyTest

--- parent/cxf/src/main/java/ch/test/cxf/BeachBoyTest.java
+++ parent/cxf/src/main/java/ch/test/cxf/BeachBoyTest.java
@@ -13,9 +13,37 @@ public class BeachBoyTest {
 	private String simpleSearchAPI = "http://172.19.9.106/custprofile/SimpleSearch.json";
 	public static void main(String[] args){
 		BeachBoyTest test = new BeachBoyTest();
-		String jsonSearchString = test.jsonSearchString("11111");
-		System.out.println(test.freeIndexSearch(jsonSearchString));
-		System.out.println(test.simpleSearchAPI(null, jsonSearchString));
+		String jsonSearchString = test.jsonSearchString("6000000", "68090000");
+		String freeIndexSearch = test.freeIndexSearch(jsonSearchString);
+		String simpleSearchAPI = test.simpleSearchAPI(null, jsonSearchString);
+		System.out.println(nodeEqual(toNode(freeIndexSearch).get("Result"), toNode(simpleSearchAPI).get("Result")));
+	}
+	private static JsonNode toNode(String json){
+		System.out.println(json);
+		try {
+			return new ObjectMapper().readTree(json);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	private static boolean nodeEqual(JsonNode node1, JsonNode node2){
+		if (node1.isArray() && node2.isArray()){
+			for (int i1 = 0; i1 < node1.size(); i1++){
+				boolean equals = false;
+				for (int i2 = 0; i2 < node2.size(); i2++)
+					if (node1.get(i1).equals(node2.get(i2))){
+						equals = true;
+						break;
+					}
+				if(!equals)
+					return equals;
+			}
+			return true;
+		} else {
+			return node1.equals(node2);
+		}
 	}
 	private String jsonSearchString(String... custNos){
 		java.util.List<String> custNoList = java.util.Arrays.<String>asList(custNos);
@@ -60,7 +88,11 @@ public class BeachBoyTest {
 			String key = entry.getKey();
 			if (key.equals("queryString"))
 				key = "mainQuery";
+			if (key.equals("fieldMap"))
+				key = "displayField";
 			node.set(key, entry.getValue());
+			if (key.equals("index") && "custprofile".equals(entry.getValue().asText()))
+				node.put(key, "custProfile");
 		}
 		try {
 			jsonSearchString = mapper.writeValueAsString(node);
@@ -69,7 +101,7 @@ public class BeachBoyTest {
 		}
 		Map<String, String> paramMap = new HashMap<String, String>();
 		paramMap.put("Q", jsonSearchString);
-		System.out.println("simpleSearchAPI = " + jsonSearchString);
+		System.out.println("simpleSearchAPI = " + paramMap);
 		try {
 			return HttpPostClient.postMethod(simpleSearchAPI, paramMap);
 		} catch (HttpException e) {
